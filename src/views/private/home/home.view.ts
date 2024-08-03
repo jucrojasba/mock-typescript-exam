3f1e7744120e2a72ca08b0bfffce097421761329
Juan Camilo Rojas <jucrojasba@gmail.com>
2024-08-03 13:33:24 -0500

    Add background home container and home styles

--- src/views/private/home/home.view.ts
+++ src/views/private/home/home.view.ts
@@ -1,15 +1,26 @@
 import { loader } from "../../../components/loader/loader.component.ts";
 import { navbar } from "../../../components/nabar/navbar.component.ts";
+import './home.view.css'
 
 export function homeView() {
-  //Erase last page viewed
+  //Erase last page viewed & load navbar
+  loader(true);
   const $root = document.getElementById("root") as HTMLElement;
   $root.innerHTML = "";
-
-  //Cargar navBar al inicio del HTML
-  loader(true);
   navbar();
   loader(false);
+
+  //HTMLElements
+  const $homeContainer = document.createElement('div') as HTMLDivElement;
+
+  //Content
+
+
+  //Classes and IDs
+  $homeContainer.classList.add('home-container')
+
+  //Jerarquia de Etiquetas HTML
+  $root.appendChild($homeContainer);
 
 
 }
