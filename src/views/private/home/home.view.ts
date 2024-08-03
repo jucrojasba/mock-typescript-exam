a7c5ac2a9740c3fdb7ca12cc9b3b113832d667f3
Juan Camilo Rojas <jucrojasba@gmail.com>
2024-08-03 13:08:48 -0500

    Add navBar component and controller for logging outin user.controller.ts

--- src/views/private/home/home.view.ts
+++ src/views/private/home/home.view.ts
@@ -1,4 +1,15 @@
+import { loader } from "../../../components/loader/loader.component.ts";
+import { navbar } from "../../../components/nabar/navbar.component.ts";
+
 export function homeView() {
+  //Erase last page viewed
   const $root = document.getElementById("root") as HTMLElement;
-  $root.innerHTML = "Hola desde Home";
+  $root.innerHTML = "";
+
+  //Cargar navBar al inicio del HTML
+  loader(true);
+  navbar();
+  loader(false);
+
+
 }
